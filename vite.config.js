afdc48c2c9cebb3eb6f9616083dca9b129e8c0e4
Mahiuha <tiasho.mahiuha@gmail.com>
2023-08-11 14:48:19 +0300

    configuring deploy file

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ command }) => {
   const config = {
     plugins: [react()],
+    build: {
+
+	outDir: 'public',
+
+	assetsDir: 'build',
+
+	publicDir: 'assets'
+
+    },
     base:'/',
   }
 
