94a507ffdcd67bbae0246a23267c92d34d8a6530
Mahiuha <tiasho.mahiuha@gmail.com>
2023-10-07 22:47:58 +0300

    made changes to timeline

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -18,13 +18,13 @@ export default [
         details: 'Fundations of Computer Science, including Linux, data structures, algorithms, low-level and high level programming languages, databases, APIs, and DevOps.',
     },
     {
-        year: '2017',
+        year: '2021',
         title: 'Associate degree in Information Technology (Education)',
         duration: '2 years',
         details: 'Technical background using different technologies and solving real problems',
     },
     {
-        year: '2014',
+        year: '2017',
         title: 'First Line of Code',
         duration: 'The beginning',
         details: 'I wrote my first line of code using Assembly to make the popular hello world and it seems so simple but at the same time so awesome to give some instruction to the computer and see it printed on the screen, and then it begin my curiosity and passion for programming and things related to technology.',
